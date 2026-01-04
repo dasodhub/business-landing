b0e7431b096fc96b2ce1efbb197fe271b640647f
Dasodhub Technologies <online-sales-group@Dasodhub-MacBook-Pro.local>
2026-01-03 23:57:42 -0800

    Defined routes in the PageController

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -7,6 +7,20 @@ use Illuminate\Http\Request;
 class PageController extends Controller
 {
     public function home() {
-        
+        return view('pages.home');
     }
+
+    public function about() {
+        return view('pages.about');
+    }
+
+    public function services() {
+        return view('pages.services');
+    }
+
+    public function contact() {
+        return view('pages.contact');
+    }
+
+
 }
