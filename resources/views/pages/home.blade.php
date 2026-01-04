8e801952950ecd07693ff354a8cd442ce99651a2
Dasodhub Technologies <online-sales-group@Dasodhub-MacBook-Pro.local>
2026-01-04 00:54:27 -0800

    Add the hero into partials and included in the home file

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,7 +2,6 @@
 @section('title', 'Home')
 
 @section('content')
-    <h1>Welcome to Our Business Landing Page</h1>
-    <p>Your success starts here. Explore our services and offerings designed to help your business thrive.</p>
+@include('partials.hero')
 
 @endsection
