5a3720a39d4779a7c83880ee3c84957ab937d1fd
Dasodhub Technologies <online-sales-group@Dasodhub-MacBook-Pro.local>
2026-01-03 23:52:35 -0800

    add pages and extended the master layout and also add the PageController

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master')
+@section('title', 'About Us')
+
+@section('content')
+    <h1>About Us</h1>
+    <p>We are a team of dedicated professionals committed to providing top-notch services to our clients. With years of experience in the industry, we have built a reputation for excellence and customer satisfaction.</p>
+@endsection
---
+++ resources/views/pages/services.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.master')
+
+@section('title', 'Services')
+
+@section('content')
+    <h1>Services</h1>
+    <p>We offer a range of services to help your business grow and succeed. Whether you need help with marketing, sales, or business development, our team of experts is here to support you every step of the way.</p>
+@endsection
